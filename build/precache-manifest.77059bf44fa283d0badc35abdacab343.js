ea1e8364c4f72306dd612d7cace2ee5a7e5ae70b
TheWolfKid <120409583+TheWolfKid@users.noreply.github.com>
2023-06-12 08:30:23 -0700

    Add files via upload

---
+++ build/precache-manifest.77059bf44fa283d0badc35abdacab343.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "27ce34aea3d31d9511de",
+    "url": "/static/css/main.43eff531.chunk.css"
+  },
+  {
+    "revision": "27ce34aea3d31d9511de",
+    "url": "/static/js/main.4701d873.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7e2385774e9572b44df5",
+    "url": "/static/css/2.4d388378.chunk.css"
+  },
+  {
+    "revision": "7e2385774e9572b44df5",
+    "url": "/static/js/2.80f4918b.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ec03d258afb20a47ecc47ce2eefa92f0",
+    "url": "/index.html"
+  }
+];
